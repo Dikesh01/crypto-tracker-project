647b5f7c55e0513aff31e3aa2854d0a35b330337
kabir33559@gmail.com <Dikesh01>
2023-09-23 11:53:02 +0530

    submission

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,16 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Switch } from "@mui/material";
 import "./style.css";
 import TemporaryDrawer from './drawer';
 import Button from '../Button';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") == "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+  }, []);
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed!");
+    const mode = localStorage.getItem("theme");
+    if (mode == "dark") {
+      setLight();
+    } else {
+      setDark();
+    }
+  };
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
   return (
     <div className='navbar'>
       <h1 className="logo">
         Crypto Tracker<span style={{color:"var(--grey)"}}> _ _</span>
       </h1>
       <div className="links">
+
+      <Switch
+          checked={darkMode}
+          onClick={() => {
+            changeMode();
+          }}
+        />
+
         <Link to='/'>
             <p className='link'>Home</p>
         </Link>
